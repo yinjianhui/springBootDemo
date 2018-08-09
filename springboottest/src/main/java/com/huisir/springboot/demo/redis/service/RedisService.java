1de469b78299d56b7cd24a21ad65331f73e40c82
jianhui.yin <jianhui.yin@shuzun.net>
2018-08-09 23:17:10 +0800

    集成redis, 配置redisconfig

--- springboottest/src/main/java/com/huisir/springboot/demo/redis/service/RedisService.java
+++ springboottest/src/main/java/com/huisir/springboot/demo/redis/service/RedisService.java
@@ -1,6 +1,12 @@
 package com.huisir.springboot.demo.redis.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.huisir.springboot.demo.redis.config.RedisUtil;
+
 public class RedisService {
 	
+	@Autowired
+	RedisUtil redisUtil;
 
 }
